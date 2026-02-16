954b5c12122bf59bff4cc3c63db51809e125b1e5
Eduardo <edusillva784@gmail.com>
2026-02-16 10:33:19 -0300

    feat: add console app

--- MarkdownToAnki/MarkdownToAnki.ConsoleApp/Program.cs
+++ MarkdownToAnki/MarkdownToAnki.ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using MarkdownToAnki.Infrastructure.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"--> Command line arguments received:\n{args}");
+        if(args.Length == 0)
+        {
+            Console.WriteLine("--> InputPath is required and OutputPath is optional");
+            Environment.Exit(0);
+        }
+        string filePath = args[0];
+        string outputPath = args.Length == 2 ? args[1] : Path.Join(Directory.GetCurrentDirectory(),$"ankiDeck_{DateTime.Now}");
+        IMarkdownParserService parser = new MarkdownParserService();
+        IAnkiGeneratorService generator = new AnkiGeneratorService();
+        generator.GenerateApkg(parser, outputPath);
+        Console.WriteLine($"--> Deck generated at: {outputPath}");
+    }
+}
+
+
