58de47aea2e43c4bb434c1895e08c17663ffd79f
Eduardo <edusillva784@gmail.com>
2026-02-16 09:57:58 -0300

    feat: add models file

--- MarkdownToAnki/MarkdownToAnki.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MarkdownToAnki.Domain;
-
-public class Class1
-{
-
-}
---
+++ MarkdownToAnki/MarkdownToAnki.Domain/Models/MarkdownHeader.cs
@@ -0,0 +1,21 @@
+﻿namespace MarkdownToAnki.Domain.Models;
+
+public class DeckDefinition
+{
+    public string DeckName {get; set;}
+    public string Source {get; set;}
+    public string Separator {get; set;}
+}
+
+public class TemplateDefinition
+{
+    public string Name {get; set;}
+    public List<string> Fields {get; set;} = new();
+    public string Usage {get; set;}
+}
+
+public class FlashCardNote{
+    public TemplateDefinition Template {get; set;}
+    public Dictionary<string,string> FieldValues {get; set;} = new();
+    public List<string> Tags {get;set;} = new();
+}
