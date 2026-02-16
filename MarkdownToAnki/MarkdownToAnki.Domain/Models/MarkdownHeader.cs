c6a08fce36f94a764575ea134b5fdc0a1116f0cf
Eduardo <edusillva784@gmail.com>
2026-02-16 11:38:45 -0300

    feat: added missing fields anki.NET

--- MarkdownToAnki/MarkdownToAnki.Domain/Models/MarkdownHeader.cs
+++ MarkdownToAnki/MarkdownToAnki.Domain/Models/MarkdownHeader.cs
@@ -1,19 +1,24 @@
 ﻿namespace MarkdownToAnki.Domain.Models;
 
+public class TemplateDefinition
+{
+    public string Name {get; set;}
+    public List<string> Fields {get; set;} = new();
+    public string HtmlQuestionFormat {get; set;}
+    public string HtmlAnswerFormat {get; set;}
+    public string CssFormat {get; set;}
+
+    public string Usage {get; set;}
+}
+
 public class DeckDefinition
 {
     public string DeckName {get; set;}
     public string Source {get; set;}
     public string Separator {get; set;}
+    public List<TemplateDefinition> Templates {get; set;} = new();
 }
 
-public class TemplateDefinition
-{
-    public string Name {get; set;}
-    public List<string> Fields {get; set;} = new();
-    public string HtmlFormat {get; set;}
-    public string Usage {get; set;}
-}
 
 public class FlashCardNote{
     public TemplateDefinition Template {get; set;}
